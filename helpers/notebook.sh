e95933ed8f20ea3b77a8d1c6686c8b17b85621a6
Bakeforfun <Baker@bakeforfun.local>
2016-11-27 20:50:45 +0300

    XGBoost tuning + script added
    Initialisation script for AWS added

---
+++ helpers/notebook.sh
@@ -0,0 +1,27 @@
+#!/usr/bin/env bash
+
+cd ~
+wget https://repo.continuum.io/archive/Anaconda3-4.2.0-Linux-x86_64.sh
+bash Anaconda3-4.2.0-Linux-x86_64.sh -b
+echo 'PATH="/home/ubuntu/anaconda3/bin:$PATH"' >> .bashrc
+. .bashrc
+
+jupyter notebook --generate-config
+
+key=$(python -c "from notebook.auth import passwd; print(passwd())")
+
+cd ~
+mkdir certs
+cd certs
+certdir=$(pwd)
+openssl req -x509 -nodes -days 365 -newkey rsa:1024 -keyout mycert.key -out mycert.pem
+
+cd ~
+sed -i "1 a\
+c = get_config()\\
+c.NotebookApp.certfile = u'$certdir/mycert.pem'\\
+c.NotebookApp.keyfile = u'$certdir/mycert.key'\\
+c.NotebookApp.ip = '*'\\
+c.NotebookApp.open_browser = False\\
+c.NotebookApp.password = u'$key'\\
+c.NotebookApp.port = 8888" .jupyter/jupyter_notebook_config.py
