07d54f14bc04a4472179ab554ff573f0cd1cbb65
Bakeforfun <Baker@bakeforfun.local>
2016-11-27 22:42:46 +0300

    Minor-2

--- scripts/initialisation.sh
+++ scripts/initialisation.sh
@@ -4,7 +4,7 @@ cd ~
 wget https://repo.continuum.io/archive/Anaconda3-4.2.0-Linux-x86_64.sh
 bash Anaconda3-4.2.0-Linux-x86_64.sh -b
 echo 'PATH="/home/ubuntu/anaconda3/bin:$PATH"' >> .bashrc
-. .bashrc
+. ~/.bashrc
 
 sudo apt-get -y install make
 sudo apt-get -y update
--- scripts/notebook.sh
+++ scripts/notebook.sh
@@ -4,7 +4,7 @@ cd ~
 wget https://repo.continuum.io/archive/Anaconda3-4.2.0-Linux-x86_64.sh
 bash Anaconda3-4.2.0-Linux-x86_64.sh -b
 echo 'PATH="/home/ubuntu/anaconda3/bin:$PATH"' >> .bashrc
-cd ~; . .bashrc
+. ~/.bashrc
  
 jupyter notebook --generate-config
 
